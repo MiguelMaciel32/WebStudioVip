39f42bf71c557a3850f840121bb2677b04ab3559
Luís Miguel <157251097+MiguelMaciel32@users.noreply.github.com>
2024-06-06 21:59:49 -0300

    section update

--- components/AgendamentoTrigger.tsx
+++ components/AgendamentoTrigger.tsx
@@ -49,14 +49,14 @@ export default function AgendamentoTrigger({
             eficiente.
           </SheetDescription>
         </SheetHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
+        <section className="grid gap-4 py-4">
+          <section className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="name" className="text-right">
               Nome
             </Label>
             <Input id="name" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
+          </section>
+          <section className="grid grid-cols-4 items-center gap-4">
             <Label htmlFor="username" className="text-right">
               Telefone
             </Label>
@@ -65,14 +65,14 @@ export default function AgendamentoTrigger({
               Data
             </label>
             <Input className="w-full col-span-2" type="date" />
-          </div> 
-          <div className="flex">
+          </section> 
+          <section className="flex">
             <Label className="text-right mx-2 mt-3">
               Horario
             </Label>
             <Input type="time" className="w-full col-span-2" />
-          </div>
-          <div className="flex">
+          </section>
+          <section className="flex">
             <Label className="text-right mx-1 mt-2">
               Notificação
             </Label>
@@ -87,8 +87,8 @@ export default function AgendamentoTrigger({
                 </SelectContent>
               </Select>
 
-          </div>
-        </div>
+          </section>
+        </section>
         <SheetFooter>
           <SheetClose asChild>
             <Button type="submit" onClick={() => {
