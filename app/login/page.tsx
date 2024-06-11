24eb15faa2954d5d4bfeedb5f57e1b219f6848d5
Luís Miguel <157251097+MiguelMaciel32@users.noreply.github.com>
2024-06-11 13:58:43 -0700

    feat: add toast in login button action

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,11 +4,11 @@ import Image from "next/image";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
 
+import { toast } from "@/components/ui/use-toast";
 
 export default function Login() {
   return (
     <main className="flex w-full h-screen overflow-y-hidden">
-      
       <section className="flex flex-col w-1/2  gap-5 container p-16 justify-center grow md:w-full">
         <h1 className="font-bold flex justify-center text-3xl tracking-tighter leading-none text-center md:text-5xl">
           Conecte-se!
@@ -22,9 +22,19 @@ export default function Login() {
           <Input placeholder="Senha" type="password" />
         </form>
         <section className="flex flex-col gap-2 justify-center">
-          <Button>Login</Button>
+          <Button
+            onClick={() => {
+              toast({
+                title: "Bem-vindo de volta!",
+              });
+            }}
+          >
+            Login
+          </Button>
           <Link className="w-full flex" href={"/login/business"}>
-          <Button variant={"secondary"} className="w-full">Entrar com uma conta empresarial</Button>
+            <Button variant={"secondary"} className="w-full">
+              Entrar com uma conta empresarial
+            </Button>
           </Link>
           <Link href={"/signup"}>
             <Button variant={"link"} className="w-full text-center">
