bdeaec74288739dbe61f7a27abc96b91070da77c
MiguelMaciel32 <157251097+MiguelMaciel32@users.noreply.github.com>
2024-06-18 16:06:12 -0700

    feat: improve

--- utils/providers/index.tsx
+++ utils/providers/index.tsx
@@ -1,9 +1,9 @@
 import { UtilsProvider } from "../context/loginContext";
 
-export default function ClientSideProviders({
-  children,
-}: {
-  children: React.ReactElement;
-}) {
-  return <UtilsProvider>{children}</UtilsProvider>;
+export default function ClientProviders({ children }: {  children: React.ReactNode}) {
+    return (
+      <UtilsProvider>
+        {children}
+      </UtilsProvider>
+    )
 }
