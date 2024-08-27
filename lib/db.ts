cd355dbd89539d353f28da5b4574c44d29a2bb35
Luís Miguel <157251097+MiguelMaciel32@users.noreply.github.com>
2024-08-27 18:07:29 -0300

    att

--- lib/db.ts
+++ lib/db.ts
@@ -1,18 +1,18 @@
 import mysql from 'mysql2/promise';
 export const createConnection = async () => {
   const connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password:  '723616Ll#',
-    database: 'studiovip'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
   });
   return connection;
 };
 
-// Tipo para resultados de consulta que são sempre arrays de dados
+
 type SelectQueryResult<T = any> = T[];
 
-// Função para consultas que retornam arrays de dados
+
 export const query = async <T = any>(sql: string, values: any[] = []): Promise<SelectQueryResult<T>> => {
   const connection = await createConnection();
   try {
